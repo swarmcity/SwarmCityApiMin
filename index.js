6bc3f177eccb2fe612ee1c662b9ef11c46b1afbb
Will in China <email.will.in.china@gmail.com>
2018-08-24 11:05:04 +0800

    dot env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env nodejs
+require('dotenv').config()
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(3000, 'localhost');
-console.log('Server running at http://localhost:3000/');
+}).listen(process.env.PORT, 'localhost');
+console.log(`Server running at http://localhost:${process.env.PORT}/`);
