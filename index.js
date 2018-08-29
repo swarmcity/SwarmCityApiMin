dbc4618aa870b8132a2150af32ab51f4c2ec31ec
Will in China <email.will.in.china@gmail.com>
2018-08-29 14:22:13 +0800

    post body

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const ipfsAPI = require('ipfs-api')
 const ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'})
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/:id', function (req, res) {
     var id = req.params.id;
     res.send(`Hello World! ${id}`)
 })
 app.post('/', function (req, res) {
-    res.send('Got a POST request')
+    const body = req.body;
+    res.send(`Got a POST request: ${body}`)
 })
 
 
