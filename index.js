72639e3f1b3a10f2f127d4b666ae0fba3ab055bd
Will in China <email.will.in.china@gmail.com>
2018-08-29 13:37:40 +0800

    method test

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 #!/usr/bin/env nodejs
 require('dotenv').config()
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(process.env.PORT, 'localhost');
-console.log(`Server running at http://localhost:${process.env.PORT}/`);
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const ipfsAPI = require('ipfs-api')
+const ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'})
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+})
+app.put('/', function (req, res) {
+    res.send('Got a PUT request')
+})
+app.delete('/', function (req, res) {
+    res.send('Got a DELETE request')
+})
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(process.env.PORT, function(){
+    console.log(`Server running at http://localhost:${process.env.PORT}/`);
+});
