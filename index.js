774745d52d2ce34de72b93df3c835cf07023fabe
Will in China <email.will.in.china@gmail.com>
2018-08-30 20:15:25 +0800

    moar

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 #!/usr/bin/env nodejs
-// version 1.0
 require('dotenv').config()
-
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
 const ipfsAPI = require('ipfs-api')
 const ipfs = ipfsAPI('127.0.0.1', '5001', {protocol: 'http'})
 const bodyParser = require("body-parser");
 const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -24,27 +20,16 @@ app.post('/ipfs/', (req, res) => {
     .then((response) => res.send({success: true, hash: response[0].hash}))
     .catch((err) => res.send({success: false, error: err}))
 })
-
 app.get('/ipfs/:data', (req, res) => {
     ipfs.files.cat(req.params.data)
     .then((response) => res.send({success: true, data: Buffer(response, 'ascii').toString('utf8')}))
     .catch((err) => res.send({success: false, error: err}))
 })
-
 app.get('/img/:data', (req, res) => {
     ipfs.files.cat(req.params.data)
     .then((response) => res.send(Buffer(response, 'ascii').toString('utf8')))
     .catch((err) => res.send({success: false, error: err}))
 })
-
 app.get('*', (req, res) => res.send('Welcome to the Swarm City API'))
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-});
-
-http.listen(process.env.PORT, function(){
-    console.log(`Server running at http://localhost:${process.env.PORT}/`);
-});
-
-module.exports = app;
+http.listen(process.env.PORT);
