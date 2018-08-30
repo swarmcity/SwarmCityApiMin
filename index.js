7d532f3a180eb6a54cb41b1f344b42f011de291f
Will in China <email.will.in.china@gmail.com>
2018-08-30 19:05:47 +0800

    compress

--- index.js
+++ index.js
@@ -19,54 +19,25 @@ app.use(compress());
 app.use(helmet());
 app.use(cors())
 
-
 app.post('/ipfs/', (req, res) => {
     ipfs.add(new Buffer(req.body.data, 'utf8'))
-    .then((response) => {
-        res.send({
-            success: true, 
-            hash: response[0].hash,
-        });
-    })
-    .catch((err) => {
-        res.send({
-            success: false,
-            error: err
-        });
-    })
+    .then((response) => res.send({success: true, hash: response[0].hash}))
+    .catch((err) => res.send({success: false, error: err}))
 })
 
 app.get('/ipfs/:data', (req, res) => {
     ipfs.files.cat(req.params.data)
-    .then((response) => {
-        res.send({
-            success: true,
-            data: Buffer(response, 'ascii').toString('utf8'),
-        });
-    })
-    .catch((err) => {
-        res.send({
-            success: false,
-            error: err
-        });
-    })
+    .then((response) => res.send({success: true, data: Buffer(response, 'ascii').toString('utf8')}))
+    .catch((err) => res.send({success: false, error: err}))
 })
 
 app.get('/img/:data', (req, res) => {
     ipfs.files.cat(req.params.data)
-    .then((response) => {
-        res.send(Buffer(response, 'ascii').toString('utf8'));
-    })
-    .catch((err) => {
-        res.send({
-            success: false,
-            error: err
-        });
-    })
+    .then((response) => res.send(Buffer(response, 'ascii').toString('utf8')))
+    .catch((err) => res.send({success: false, error: err}))
 })
 
 app.get('*', (req, res) => res.send('Welcome to the Swarm City API'))
-
 
 io.on('connection', function(socket){
     console.log('a user connected');
